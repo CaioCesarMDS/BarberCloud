2513afba1621dcc7384d87f16557996b88f6d02b
CaioCesarMDS <caiocesarsts@gmail.com>
2025-05-26 23:14:17 -0300

    feat: add forms with validation using React Hook Form and Zod

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,53 +1,124 @@
-import Link from "next/link";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import Header from "../_components/header";
 import { Button } from "../_components/shadcn/ui/button";
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../_components/shadcn/ui/card";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../_components/shadcn/ui/form";
 import { Input } from "../_components/shadcn/ui/input";
-import { Label } from "../_components/shadcn/ui/label";
+import api from "../services/api";
 
-const SignIn = () => {
+const formSchema = z.object({
+  name: z.string().min(4, { message: "Name must be at least 4 characters." }),
+  phone: z.string().min(8, { message: "Phone must be at least 9 characters." }),
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z.string().min(6, { message: "Password must be at least 8 characters." }),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+const SignUp = () => {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      phone: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const response = await api.post("/auth/signup", data);
+      if (response.status === 201) {
+        console.log("User signed up successfully:", response.data);
+      } else {
+        console.error("Error signing up:", response.data);
+      }
+    } catch (error) {
+      console.error("Error during sign up:", error);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex flex-col">
+    <div>
       <Header />
-      <div className="grid place-items-center flex-1">
-        <Card className="w-11/12 max-w-md">
-          <CardHeader className="text-2xl">
-            <CardTitle>Sign Up</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="email">Email</Label>
-                  <Input id="email" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">Password</Label>
-                  <Input id="password" />
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Link href="/signin">
-              <Button variant="outline">Sign In</Button>
-            </Link>
-            <Button>Enter</Button>
-          </CardFooter>
-        </Card>
-      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col items-center justify-center min-h-screen"
+        >
+          <div className="w-10/12 max-w-md space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button type="submit" className="mt-6 mx-auto w-1/3">
+            Sign Up
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
 
-export default SignIn;
+export default SignUp;
