a41fcfb42156ed587a1d6009bab44f2593d6b6f0
CaioCesarMDS <caiocesarsts@gmail.com>
2025-05-26 23:11:46 -0300

    feat: configure integration between frontend and backend with axios and CORS

--- backend/src/auth/dtos/auth.dto.ts
+++ backend/src/auth/dtos/auth.dto.ts
@@ -2,35 +2,35 @@ import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class SignUpDTO {
   @IsNotEmpty()
-  name: string;
+  name!: string;
 
   @IsNotEmpty()
   @MinLength(9)
-  phone: string;
+  phone!: string;
 
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsNotEmpty()
   @MinLength(8)
-  password: string;
+  password!: string;
 }
 
 export class SignInDTO {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsNotEmpty()
-  password: string;
+  password!: string;
 }
 
 export class AuthResponseDTO {
-  id: string;
-  name: string;
-  email: string;
-  phone: string;
+  id!: string;
+  name!: string;
+  email!: string;
+  phone!: string;
 }
 
 export class AuthTokenDTO {
-  accessToken: string;
+  accessToken!: string;
 }
