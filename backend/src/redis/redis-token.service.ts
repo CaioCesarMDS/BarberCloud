cdd7b2f5b19318ad358468a2f6e850713c53c48b
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-06-10 11:47:39 -0300

    fix: redis connection

--- backend/src/redis/redis-token.service.ts
+++ backend/src/redis/redis-token.service.ts
@@ -1,24 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import Redis from 'ioredis';
+import { RedisService } from './redis.service';
 
 @Injectable()
-export class RedisTokenService {
-  private redis = new Redis();
+export class RedisTokenService implements OnModuleInit {
+  private client!: Redis;
+
+  constructor(private readonly redisService: RedisService) {}
+
+  onModuleInit() {
+    this.client = this.redisService.getClient()
+  }
 
   async generateResetCode(email: string): Promise<string> {
     const code = Math.floor(100000 + Math.random() * 900000).toString();
     const key = `reset:${email}`;
-    await this.redis.set(key, code, 'EX', 15 * 60);
+    await this.client.set(key, code, 'EX', 15 * 60);
     return code;
   }
 
   async getResetCode(email: string): Promise<string | null> {
     const key = `reset:${email}`;
-    return await this.redis.get(key);
+    return await this.client.get(key);
   }
 
   async invalidateCode(email: string): Promise<void> {
     const key = `reset:${email}`;
-    await this.redis.del(key);
+    await this.client.del(key);
   }
 }
