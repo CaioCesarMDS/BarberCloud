f4b8ee5a89928d1d2e5a5518f3e5e295cfd95ee9
CaioCesarMDS <caiocesarsts@gmail.com>
2025-06-10 11:18:36 -0300

    feat: add inputmask

--- frontend/app/_components/header.tsx
+++ frontend/app/_components/header.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./shadcn/ui/button";
 import { Card, CardContent } from "./shadcn/ui/card";
-// comentando pra o windows ver que eu mudei essa merda
+
 export default function Header() {
   return (
     <Card>
       <CardContent className="px-5 py-4 flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="IF Barber logo" width={60} height={60} />
+          <Image src="/logo.png" alt="IF Barber logo" width={50} height={50} />
         </Link>
         <Button variant="outline" size="icon">
           <MenuIcon size={18} />
