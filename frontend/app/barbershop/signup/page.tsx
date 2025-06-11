9409a5de016535999b0d3f01e85a198399a0639b
CaioCesarMDS <caiocesarsts@gmail.com>
2025-06-10 21:29:33 -0300

    feat: add MaskedInputField component and change datepicker

--- frontend/app/barbershop/signup/page.tsx
+++ frontend/app/barbershop/signup/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { InputFieldMask } from "@/app/_components/form/fields/InputFieldMask";
+import DatePickerField from "@/app/_components/form/fields/DatePickerField";
+import { MaskedInputField } from "@/app/_components/MaskedInputField";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import DatePickerField from "../../_components/form/fields/DatePickerField";
 import InputField from "../../_components/form/fields/InputField";
 import FormWrapper from "../../_components/form/FormWrapper";
 import Header from "../../_components/Header";
@@ -20,7 +20,8 @@ const formSchema = z
     phone: z
       .string()
       .min(0, { message: "Phone must be at least 10 characters." })
-      .max(15, { message: "phone must have a maximum of 10 characters." }),
+      .max(15, { message: "phone must have a maximum of 10 characters." })
+      .trim(),
     email: z.string().email({ message: "Invalid email address." }),
     password: z
       .string()
@@ -145,12 +146,9 @@ export default function SignUp() {
         <h3 className="p-6">Sign Up</h3>
 
         <InputField control={form.control} name="name" label="Name" type="text" />
-        <InputFieldMask control={form.control} name="phone" label="phone" type="tel" mask="+55 (99) 99999-9999" />
-        {/* <InputField control={form.control} name="phone" label="Phone" type="tel" /> */}
+        <MaskedInputField control={form.control} name="phone" label="Phone" mask="+55 00 00000-0000" />
         <InputField control={form.control} name="email" label="Email" type="email" />
-
-        <DatePickerField control={form.control} name="birth" label="Date Of Birth" />
-
+        <DatePickerField control={form.control} name="birth" label="Birth Date"/>
         <InputField control={form.control} name="password" label="Password" type="password" />
         <InputField control={form.control} name="confirmPassword" label="Confirm Password" type="password" />
 
--- frontend/app/client/signup/page.tsx
+++ frontend/app/client/signup/page.tsx
@@ -1,43 +1,43 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import DatePickerField from "../../_components/form/fields/DatePickerField";
+import DatePicker from "../../_components/form/fields/DatePickerField";
 import InputField from "../../_components/form/fields/InputField";
 import FormWrapper from "../../_components/form/FormWrapper";
-import Header from "../../_components/header";
+import Header from "../../_components/Header";
 import api from "../../services/api";
-import { useRouter } from "next/navigation";
 
 const formSchema = z
-.object({
-  name: z.string().min(4, { message: "Name must be at least 4 characters." }),
-  birth: z.date({ message: "Invalid date." }),
-  phone: z.string().min(10, { message: "Phone must be at least 10 characters." }),
-  email: z.string().email({ message: "Invalid email address." }),
-  password: z
-  .string()
-  .min(8, { message: "Be at least 8 characters long" })
-  .regex(/[a-z]/, { message: "Contains at least one lowercase letter." })
-  .regex(/[A-Z]/, { message: "Contains at least one uppercase letter." })
-  .regex(/[0-9]/, { message: "Contain at least one number." })
-  .regex(/[^a-zA-Z0-9]/, {
-    message: "Contain at least one special character.",
+  .object({
+    name: z.string().min(4, { message: "Name must be at least 4 characters." }),
+    birth: z.date({ message: "Invalid date." }),
+    phone: z.string().min(10, { message: "Phone must be at least 10 characters." }),
+    email: z.string().email({ message: "Invalid email address." }),
+    password: z
+      .string()
+      .min(8, { message: "Be at least 8 characters long" })
+      .regex(/[a-z]/, { message: "Contains at least one lowercase letter." })
+      .regex(/[A-Z]/, { message: "Contains at least one uppercase letter." })
+      .regex(/[0-9]/, { message: "Contain at least one number." })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Contain at least one special character.",
       })
       .trim(),
-      confirmPassword: z.string().min(8, { message: "Confirm Password must be at least 8 characters." })
+    confirmPassword: z.string().min(8, { message: "Confirm Password must be at least 8 characters." }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
-  
+
 type FormData = z.infer<typeof formSchema>;
 
 export default function SignUp() {
   const router = useRouter();
-  
+
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -54,7 +54,7 @@ export default function SignUp() {
     try {
       const userData = { ...data, birth: data.birth.toISOString(), confirmPassword: undefined };
 
-      console.log(userData.birth)
+      console.log(userData.birth);
 
       const response = await api.post("/auth/client/signup", userData);
       if (response.status === 201) {
@@ -68,7 +68,7 @@ export default function SignUp() {
     }
   };
 
-  console.log(form.formState.errors)
+  console.log(form.formState.errors);
 
   return (
     <main>
@@ -78,7 +78,7 @@ export default function SignUp() {
         <InputField control={form.control} name="phone" label="Phone" type="tel" />
         <InputField control={form.control} name="email" label="Email" type="email" />
 
-        <DatePickerField control={form.control} name="birth" label="Date Of Birth" />
+        <DatePicker />
 
         <InputField control={form.control} name="password" label="Password" type="password" />
         <InputField control={form.control} name="confirmPassword" label="Confirm Password" type="password" />
