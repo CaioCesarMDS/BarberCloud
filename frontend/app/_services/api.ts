6294930f5b626b151a35a444a46c9e44b6c55214
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-07-02 17:16:42 -0300

    fix: css client profile

--- frontend/app/_services/api.ts
+++ frontend/app/_services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_URL_API,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
